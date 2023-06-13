a9c96e1ebf0a198a9436f713d9c287bd4b60a249
Lekhana2003 <2003rlekhana@gmail.com>
2023-06-13 19:15:06 +0530

    l4

--- home/urls.py
+++ home/urls.py
@@ -7,11 +7,11 @@ from . import views
 
 
 urlpatterns = [
+    path('home', views.home, name="home"),
+    path('', views.home, name="home"),
+    path('addclassroom', views.addclassroom, name="addclassroom"),
+    path('addinstructor', views.addinstructor, name="addinstructor"),
+    path('addsubjects', views.addsubjects, name="addsubjects"),
+    path('theory', views.theory, name="theory"),
     
-    path('home',views.home,name="home"),
-    path('',views.home,name="home"),
-    path('addclassroom',views.addclassroom,name="addclassroom"),
-    path('addinstructor',views.addinstructor,name="addinstructor"),
-    path('addsubjects',views.addsubjects,name="addsubjects"),
-    path('theory',views.theory,name="theory"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,55 +1,45 @@
-from django.shortcuts import render,redirect,HttpResponse
-from home.models import classroom
-from home.models import instructor
-from .models import classroom
-
-
+from django.shortcuts import render, redirect
+from home.models import classroom, instructor
 
 # Create your views here.
 def home(request):
-    #return HttpResponse("this is my home page")
     return render(request, 'home.html')
 
 def addclassroom(request):
-    context={'success':False}
-    if request.method=="POST":
-        title=request.POST['title']
-        print(title)
-        ins =classroom(classnum=title)
+    context = {'success': False}
+    if request.method == "POST":
+        title = request.POST['title']
+        ins = classroom(classnum=title)
         ins.save()
-        context={'success':True}
-    allLists=classroom.objects.all()
-    context={'addclassroom':allLists}    
-    #return HttpResponse("this is my home page")
-    return render(request, 'addclassroom.html',context)  
+        context['success'] = True
+    allLists = classroom.objects.all()
+    context['addclassroom'] = allLists
+    return render(request, 'addclassroom.html', context)
+  
 
 def theory(request):
-    return render(request,'theory.html')
+    return render(request, 'theory.html')
 
 def addinstructor(request):
-    context={'success':False}
-    if request.method=="POST":
-       instructorid = request.POST.get('instructorid')
-       name = request.POST.get('name')
-       designation = request.POST.get('designation')
-       email = request.POST.get('email')
-       phone = request.POST.get('phone')
+    context = {'success': False}
+    allLists = instructor.objects.all()
+    
+    if request.method == "POST":
+        instructorid = request.POST.get('instructorid')
+        name = request.POST.get('name')
+        designation = request.POST.get('designation')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
         
-       
-       
+        ins = instructor(instructorid=instructorid, name=name, designation=designation, email=email, phone=phone)
+        ins.save()
+        
+        allLists = instructor.objects.all()
+        context = {'success': True, 'addinstructor': allLists}
 
-       #print(name, email,phone,desc)
-       ins =instructor(instructorid=instructorid,name=name,designation=designation, email=email, phone=phone )
-       ins.save()
-       context={'success':True}
-      
-    print("The data had been return to the db")
-       
-    #return HttpResponse("this is my contact page")
-    return render(request, 'addinstructor.html')
+    return render(request, 'addinstructor.html', context)
+
 
 def addsubjects(request):
-    return render(request,'addsubjects.html')
-
-    
+    return render(request, 'addsubjects.html')
 
